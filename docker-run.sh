02a5bd58025596c43ca2c07b7c7361d28e28f2d0
Ryan Price <ryan.price@slalom.com>
2018-07-23 14:42:51 -0500

    Add volume, and put sqlcmd on container's PATH.

--- docker-run.sh
+++ docker-run.sh
@@ -1,4 +1,7 @@
-image="microsoft/mssql-server-linux"
+# Can review here how to migrate existing DBs (or backups) to MSSQL on Linux:
+# https://docs.microsoft.com/en-us/sql/linux/sql-server-linux-migrate-restore-database?view=sql-server-linux-2017
+
+image="microsoft/mssql-server-linux:latest"
 container="mssql"
 
 docker container stop $container >/dev/null && docker container rm $container >/dev/null
@@ -7,8 +10,12 @@ docker container run \
     -e 'ACCEPT_EULA=Y' \
     -e 'SA_PASSWORD=M$$QL$3RV3R' \
     -p 1433:1433 \
+   -v mssql_data:/var/opt/mssql \
     --name $container \
     -d \
     $image
+
+# Add mssql-tools to PATH
+docker container exec $container sh -c "echo 'export PATH=\"$PATH:/opt/mssql-tools/bin\"' >> /root/.bashrc"
 
 printf "MS SQL Server running on port 1433.\n"
